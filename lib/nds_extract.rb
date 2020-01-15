d25200e2291e43eb32525d9a84f59565bfc87bfc
Firuz Afzalov <afzalov98@gmail.com>
2020-01-15 00:16:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -107,7 +107,7 @@ def movies_with_directors_set(source)
   
   while index < source.length do 
     hash = source[index]
-    director_name = 
+    director_name = hash[]
     index += 1
   end
 end
