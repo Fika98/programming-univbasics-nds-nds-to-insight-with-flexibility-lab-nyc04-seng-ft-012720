e4d617fde7c6c50ddc74c6e2d32ba7a51781a862
Firuz Afzalov <afzalov98@gmail.com>
2020-01-15 00:16:11 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -103,7 +103,12 @@ def movies_with_directors_set(source)
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
   index = 0 
-  movies = 
+  movies = []
+  
+  while index < source.length do 
+    
+    index += 1
+  end
 end
 
 # ----------------    End of Your Code Region --------------------
