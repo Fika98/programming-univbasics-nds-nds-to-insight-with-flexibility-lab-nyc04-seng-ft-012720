3299eda6b1fbaa834c4181c6398040f6de32defe
Firuz Afzalov <afzalov98@gmail.com>
2020-01-15 00:19:45 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -109,7 +109,7 @@ def movies_with_directors_set(source)
     hash = source[index]
     director_name = hash[:name]
     director_movies = hash[:movies]
-    movies << movies_with_directors_set(director_name,director_movies)
+    movies << movies_with_directors_set(director_name, director_movies)
     index += 1
   end
   movies
